94836bc3d88dda78f2b5f1fd1bd6b9adba97ee7f
Gwenael95 <gwenael.marchetti-waternaux@edu.itescia.fr>
2020-12-08 17:22:45 +0100

    add env URL_MONGO, PORT;  del functions

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const app = express();
 const server = http.createServer(app);
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
-const urlmongo = "mongodb://ujrosah0spdjrzvsreq3:wHmS3T4pjF0gcYJIYGA7@bv9h1dk3wxrjako-mongodb.services.clever-cloud.com:27017/bv9h1dk3wxrjako";
+require('dotenv').config();
+
+const urlMongo = process.env.URL_MONGO;
 /* End setup webserver */
 
-mongoose.connect(urlmongo, {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect(urlMongo, {useUnifiedTopology: true, useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Erreur lors de la connexion'));
 db.once('open', function (){
@@ -25,7 +27,8 @@ app.use(cors({ origin: true, credentials: true }));
 const router = require("./router");
 router(app);
 
-const PORT = 4021;
+const PORT = process.env.PORT || 4021;
 server.listen(PORT, () => {
     console.log("started " + PORT);
+    console.log(process.env.PORT)
 });
