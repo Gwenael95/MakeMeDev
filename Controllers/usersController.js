f6e67af0b09b135a699163a5319cc14f047eccb1
Antony Correia <antony.correia@grtgaz.com>
2020-12-09 11:52:28 +0100

    update model

--- Controllers/usersController.js
+++ Controllers/usersController.js
@@ -10,11 +10,16 @@ exports.signUp = async (req, res, next) => {
     }
     const response = await addUser(user)
     return res.status(response.code).send(response.body)
-
 };
 
 exports.signIn = (req, res, next)  => {
-
+    const {user} = req.query;
+    if (
+        user == null ||
+        user == undefined
+    ) {
+        return res.status(404).send({error: "Requete vide"});
+    }
 };
 
 
--- Models/userModel.js
+++ Models/userModel.js
@@ -4,9 +4,9 @@ const {postSchema} = require("./postModel");
 exports.userSchema = new mongoose.Schema({
     userName: {type: String, required: true, unique: true},
     mail: {type: String, required: true, unique: true},
-    pseudo: {type: String, required: true, unique: true},
+    pseudo: {type: String, unique: true},
     password: {type: String, required: true},
-    avatar: {type: String, required: true},
+    avatar: {type: String, default: "https://discord.com/channels/690896147404816434/785427941127225354/786182169679757314"},
     creationDate: {type: String, required: true},
     activities: {
         like: [{type: mongoose.Types.ObjectId}],
