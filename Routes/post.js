1131f618f4e5ff8d5192f98851050802a6f67645
Gwenael95 <gwenael.marchetti-waternaux@edu.itescia.fr>
2020-12-11 14:29:44 +0100

    restore authenticate token

--- Routes/post.js
+++ Routes/post.js
@@ -4,5 +4,5 @@ const {authenticateToken} = require("../Middlewares/middleware")
 
 module.exports = (app) => {
     app.get(url + "post",  getPost);
-    app.post(url + "post", /*authenticateToken,*/ sendPost);
+    app.post(url + "post", authenticateToken, sendPost);
 };
