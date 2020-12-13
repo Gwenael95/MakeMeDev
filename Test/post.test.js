e4b8a9e157839d43a05a172b3f2dae11149e21aa
Antony Correia <antony.correia@grtgaz.com>
2020-12-13 22:52:54 +0100

    bug test dislike

--- Test/post.test.js
+++ Test/post.test.js
@@ -47,7 +47,7 @@ describe('Post', () => {
         expect(response.body.success[0].name).toBe("test")
     });
 
-    it('should be able to update a vote into post if NEVER vote', async () => {
+    it('should be able to like a post if NEVER vote', async () => {
         const response = await request.post(url + 'post-vote')
             .set('Authorization', 'Bearer ' + newUser.body.token)
             .send({vote:1, idPost:newPost.body.success.post[0]._id})
@@ -55,6 +55,17 @@ describe('Post', () => {
         expect(response.status).toBe(200);
         expect(postCheck.body.success[0].post[2].like).toBe(3)
         expect(response.body.success.user.activities.like).toContain(newPost.body.success.post[0]._id)
+    });
+
+    it('should be able to dislike a post if NEVER vote', async () => {
+        const response = await request.post(url + 'post-vote')
+            .set('Authorization', 'Bearer ' + newUser.body.token)
+            .send({vote: -1, idPost:newPost.body.success.post[0]._id})
+        const postCheck = await request.get(url + 'post?search=[]')
+        console.log(newPost.body.success.post);
+        expect(response.status).toBe(200);
+        expect(postCheck.body.success[0].post[2].dislike).toBe(3)
+        expect(response.body.success.user.activities.dislike).toContain(newPost.body.success.post[0]._id)
     });
 
 
@@ -91,7 +102,9 @@ describe('Post', () => {
         expect(response1.status).toBe(200);
         expect(response2.status).toBe(200);
         expect(postCheck1.body.success[0].post[2].like).toBe(3)
+        expect(postCheck1.body.success[0].post[2].dislike).toBe(2)
         expect(postCheck2.body.success[0].post[2].like).toBe(2)
+        expect(postCheck2.body.success[0].post[2].dislike).toBe(3)
         expect(response1.body.success.user.activities.like).toContain(newPost.body.success.post[0]._id)
         expect(response1.body.success.user.activities.dislike.length).toBe(0)
         expect(response2.body.success.user.activities.dislike).toContain(newPost.body.success.post[0]._id)
