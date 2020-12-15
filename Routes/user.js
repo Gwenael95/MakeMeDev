66a057476a96c14cb36b125d3d539371b89136f8
Gwenael95 <gwenael.marchetti-waternaux@edu.itescia.fr>
2020-12-15 12:47:07 +0100

    change route to signin, update test and controller

--- Routes/user.js
+++ Routes/user.js
@@ -3,7 +3,7 @@ const {authenticateToken} = require("../Middlewares/middleware")
 const {url} = require("./const");
 
 module.exports = (app) => {
-    app.get(url + "users", signIn);
+    app.post(url + "user-signin", signIn);
     app.post(url + "users", signUp);
     app.post(url + "update-users", authenticateToken, updateUser);
 };
